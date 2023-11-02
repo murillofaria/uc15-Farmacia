5344b2ca75da7c36583d4228721809ef436e240f
Murillo Faria <murillofaria.contato@gmail.com>
2023-11-02 14:34:53 -0300

    criacao projeto

---
+++ src/test/java/com/farmacia/drogariaDeVerdade/DrogariaDeVerdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.drogariaDeVerdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogariaDeVerdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
