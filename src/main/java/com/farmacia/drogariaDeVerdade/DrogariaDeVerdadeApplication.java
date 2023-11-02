aed558d7c7d0e3854ef5c11bce770ff8e0816be2
Murillo Faria <murillofaria.contato@gmail.com>
2023-11-02 15:35:17 -0300

    criação do projeto

---
+++ src/main/java/com/farmacia/drogariaDeVerdade/DrogariaDeVerdadeApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.drogariaDeVerdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrogariaDeVerdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrogariaDeVerdadeApplication.class, args);
+	}
+
+}
